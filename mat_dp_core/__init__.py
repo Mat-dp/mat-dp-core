763d59616415a394cbeefbae0643fc514ba3b4b2
mark-todd <markpeter.todd@hotmail.co.uk>
2021-10-04 12:29:04 +0100

    Switched to actual resource flow from cost

--- mat_dp_core/__init__.py
+++ mat_dp_core/__init__.py
@@ -3,10 +3,9 @@ from numpy.typing import ArrayLike
 
 
 def calculate_run_matrix(
-        process_demands: ArrayLike,     # (process, resource) -> float
-        demand_policy: ArrayLike,       # (process, process, resource) -> +ve float
-        consumption_policy: ArrayLike   # (process, process, resource) -> +ve float
-        ) -> ArrayLike:  # run_matrix: (process, process) -> +ve float
+    process_demands: ArrayLike,     # (process, resource) -> float
+    demand_policy: ArrayLike,       # (process, process, resource) -> +ve float
+) -> ArrayLike:  # run_matrix: (process, process) -> +ve float
     """
     Given the resource demands of each process, and the demand and consumption
     policies to specify how the processes interrelate, calculate for each
@@ -16,9 +15,9 @@ def calculate_run_matrix(
 
 
 def calculate_run_scenario(
-        process_demands: ArrayLike,     # (process, resource) -> float
-        scenario: ArrayLike             # (process, process, resource) -> +ve float
-        ) -> ArrayLike:  # run_scenario: process -> +ve float
+    process_demands: ArrayLike,     # (process, resource) -> float
+    scenario: ArrayLike             # (process, process, resource) -> +ve float
+) -> ArrayLike:  # run_scenario: process -> +ve float
     """
     Given the resource demands of each process, and the scenario to specify
     the lower bound of each resource edge, calculate the lower bound of
@@ -28,9 +27,9 @@ def calculate_run_scenario(
 
 
 def calculate_run_vector(
-        run_matrix: ArrayLike,          # (process, process) -> +ve float
-        run_scenario: ArrayLike         # process -> +ve float
-        ) -> ArrayLike:  # run_vector: process -> +ve float
+    run_matrix: ArrayLike,          # (process, process) -> +ve float
+    run_scenario: ArrayLike         # process -> +ve float
+) -> ArrayLike:  # run_vector: process -> +ve float
     """
     Given the run matrix and run scenario, calculate the number of times
     that each process runs in the scenario
@@ -38,25 +37,44 @@ def calculate_run_vector(
     pass
 
 
-def calculate_cost_matrix(
-        process_demands: ArrayLike,     # (process, resource) -> float
-        run_vector: ArrayLike           # process -> +ve float
-        ) -> ArrayLike:  # cost_matrix: (process, process, resource) -> +ve float
+def calculate_actual_resource(
+    process_demands: ArrayLike,     # (process, resource) -> float
+    run_vector: ArrayLike           # process -> +ve float
+    ) -> ArrayLike:  # actual_resource: (process, resource) -> +ve float
     """
     Given the resource demands of each process, and the number of times
     that each process runs, calculate the number of resources that
-    flow through each edge
+    are required by each process.
+    """
+    pass
+
+def calculate_actual_resource_flow(
+    actual_resource: ArrayLike,     # (process, resource) -> +ve float
+    demand_policy: ArrayLike        # (process, process, resource) -> +ve float
+    ) -> ArrayLike:  # actual_resource_flow: (process, process, resource) -> +ve float
+    """
+    Given the resource demands of each process, and the number of times
+    that each process runs, calculate the number of resources that
+    are required by each process.
     """
     pass
 
 
-def measure_cost(
-        cost_slice: ArrayLike,          # (process, process) -> +ve float
-        measure: ArrayLike       # (process, process) -> bool
-        ) -> float:  # +ve float
+def get_flow_slice(
+    actual_resource_flow: ArrayLike,    # (process, process, resource) -> +ve float
+    resource_index: int
+) -> ArrayLike:     # flow slice (process, process) -> +ve float
     """
-    Given a slice of the cost matrix, which determines the number of resources
-    that flows through each edge for a single resource, calculate the total
-    number of resources that flow through measured edges
+    Get a slice of the actual resource flow given a resource index.
+    """
+    pass
+
+def measure_resource_usage(
+    flow_slice: ArrayLike,          # (process, process) -> +ve float
+    measure: ArrayLike              # (process, process) -> bool
+) -> float:  # +ve float
+    """
+    Given a flow slice, calculate the total
+    number of resources that flow through measured edges.
     """
     pass
