e56316536160927df190daa67a977001593a8d4a
mark-todd <markpeter.todd@hotmail.co.uk>
2021-10-06 19:32:01 +0100

    Cleaned code

--- mat_dp_core/maths_core.py
+++ mat_dp_core/maths_core.py
@@ -36,9 +36,7 @@ def calculate_run_matrix(
         for j, ratio in enumerate(in_process):
             if (ratio !=0 )and (i!=j):
                 run_matrix[j][i] = 1/ratio
-    print(run_matrix)
-
-    #return run_matrix
+    # Iterate to fill distant values
     new_run_matrix = np.copy(run_matrix)
     def iterate_on_run_matrix(run_matrix):
         new_run_matrix = np.copy(run_matrix)
@@ -53,15 +51,7 @@ def calculate_run_matrix(
                         if k_run_ratio!=0:
                             if new_run_matrix[i][k] ==0 and (k!=i):
                                 new_run_matrix[i][k] = run_ratio*k_run_ratio
-            """
-            for j, run_ratio in enumerate(upstream_slice):
-                if run_ratio !=0:
-                    new_slice = t_run_matrix[j]
-                    for k, k_run_ratio in enumerate(new_slice):
-                        if k_run_ratio!=0:
-                            if new_run_matrix[k][i] ==0 and (k!=i):
-                                new_run_matrix[k][i] = run_ratio*k_run_ratio
-            """
+
         return new_run_matrix
     while True:
         brand_new_run_matrix = iterate_on_run_matrix(new_run_matrix)
@@ -104,29 +94,13 @@ def calculate_run_vector(
     Given the run matrix and run scenario, calculate the number of times
     that each process runs in the scenario
     """
-    print(run_matrix)
-    print(run_scenario)
 
-
-    #Ask all edges what they predict the value of a particular node to be
-    # Take the maximum
-    # Iterate until the lower bounds stop increasing
     def evaluate_new_lower_bounds(old_lower_bounds):
         new_lower_bounds = []
         for i, current_lower_bound in enumerate(old_lower_bounds):
-            downstream_slice = run_matrix[i]
             upstream_slice = np.transpose(run_matrix)[i]
-            #current_lower_bound = old_lower_bounds[i]
             lower_bounds = [current_lower_bound]
-            """
-            for j, downstream_ratio in enumerate(downstream_slice):
-                lower_bound_at_dest = old_lower_bounds[j]
-                if downstream_ratio == 0:
-                    prediction = 0
-                else:
-                    prediction = lower_bound_at_dest/downstream_ratio
-                lower_bounds.append(prediction)
-            """
+
             for j, upstream_ratio in enumerate(upstream_slice):
                 lower_bound_at_dest = old_lower_bounds[j]
                 prediction = lower_bound_at_dest*upstream_ratio
@@ -138,126 +112,6 @@ def calculate_run_vector(
         return new_lower_bounds
     
     return evaluate_new_lower_bounds(run_scenario)
-    """
-    lower_bounds = run_scenario
-    for i in range(1):
-        new_lower_bounds = evaluate_new_lower_bounds(lower_bounds)
-        if np.array_equal(new_lower_bounds, lower_bounds):
-            break
-        lower_bounds = new_lower_bounds
-    """
-
-    
-    """
-    # TODO Figure out if this start and end method is generic - test by adding other constraints
-    start=None
-    end = None
-    for i, line in enumerate(run_matrix):
-        if all([i==0 for i in line]):
-            end = i
-    for i, line in enumerate(np.transpose(run_matrix)):
-        if all([i==0 for i in line]):
-            start = i
-    i = identity(5)
-    new_run = np.transpose(run_matrix - i)
-    test = np.delete(new_run,start, 0)
-    new_test = np.delete(test,end,1)
-    new_scenario_2 = np.delete(run_scenario,start,0)
-    out = solve(new_test, new_scenario_2)
-    print(new_test)
-    print(new_scenario_2)
-    print(out)
-    print(run_matrix)
-    """
-    """
-    run_vectors = []
-    for i, lower_bound in enumerate(run_scenario):
-        def get_submatrix_of_run_matrix(process_index: int, run_matrix: ArrayLike):
-            axis_labels = [None]*5
-
-            def get_current_line(axis_labels: ArrayLike, process_index: int):
-                proc_slice = np.transpose(run_matrix)[process_index]
-                current_line = [None]*5
-                connecting_processes = []
-                has_none_zero = False
-                for i, run_num in enumerate(proc_slice):
-                    if run_num !=0:
-                        if i not in axis_labels:
-                            first_free_slot = axis_labels.index(None)
-                            axis_labels[first_free_slot] = i
-                        line_index = axis_labels.index(i)
-                        current_line[line_index] = run_num
-                        connecting_processes.append(i)
-                        has_none_zero = True
-                    elif i == process_index:
-                        if i in axis_labels:
-                            line_index = axis_labels.index(i)
-                            current_line[line_index] = -1
-                if not has_none_zero:
-                    current_line = [None]*5
-                return current_line, axis_labels, connecting_processes
-            def get_current_lines(axis_labels, process_index):
-                current_lines = []
-                current_line, axis_labels, connecting_processes = get_current_line(axis_labels, process_index)
-                current_lines += [current_line]
-                for connected_index in connecting_processes:
-                    current_lines_new, axis_labels = get_current_lines(axis_labels, connected_index)
-                    current_lines += current_lines_new
-                return current_lines, axis_labels
-
-            current_lines, axis_labels = get_current_lines(axis_labels, process_index)
-            current_array = np.array(current_lines)
-            new_array = []
-            for line in current_array:
-                if not all([i==None for i in line]):
-                    new_array.append(line)
-            flipped_array = np.transpose(np.array(new_array))
-            new_new_array = []
-            for line in flipped_array:
-                if not all([i==None for i in line]):
-                    new_new_array.append(line)
-            
-            clean_array = np.array(new_new_array)
-            for i, item in enumerate(clean_array):
-                for j, sub_item in enumerate(item):
-                    if sub_item == None:
-                        clean_array[i][j] = 0
-            clean_array = np.transpose(np.array(clean_array, dtype = np.dtype('float64')))
-            x,y = clean_array.shape
-
-            new_axis_labels = axis_labels[0:x]
-            return clean_array, new_axis_labels
-            ""
-            # TODO Line descending tree also
-            for i, run_num in enumerate(alt_proc_slice):
-                if run_num !=0:
-                    if i not in axis_labels:
-                        axis_labels.append(i)
-                    current_line.append(run_num)
-            ""
-
-        
-        if lower_bound !=0:
-            submatrix, process_indices = get_submatrix_of_run_matrix(i, run_matrix)
-            comparison = np.array([float(0)]*len(process_indices))
-            comparison[0] = float(lower_bound)
-            results = solve(submatrix, comparison)
-            if len(results) != len(process_indices):
-                'result process index mismatch'
-            run_vector = np.zeros((len(run_scenario)))
-            for j, p_index in enumerate(process_indices):
-                result = results[j]
-                run_vector[p_index] = result
-            run_vector[i] = lower_bound
-            run_vectors.append(run_vector)
-
-
-    if len(run_vectors) == 1:
-        run_vector = run_vectors[0]
-    else:
-        raise ValueError('More than one run vector found')
-    return run_vector
-    """
 
 
 
