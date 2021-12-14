e181057d525c38be34498ecb3a3a5018073751f8
mark-todd <markpeter.todd@hotmail.co.uk>
2021-12-14 12:40:00 +0000

    Added real example constraints
    Fixed bug in resource constraint

--- examples/real_test.py
+++ examples/real_test.py
@@ -20,22 +20,25 @@ concrete_producer = processes.create("concrete_producer", (concrete, +1))
 steel_producer = processes.create("steel_producer", (steel, +1))
 aluminium_producer = processes.create("aluminium_producer", (aluminium, +1))
 electronics_producer = processes.create("electronics_producer", (electronics, +1))
+energy_producer = processes.create("energy_grid", (energy, -1))
 wind = processes.create("wind", (aluminium, -2), (concrete, -3), (energy, +1))
 solar = processes.create(
     "solar", (steel, -1), (electronics, -1), (aluminium, -0.5), (energy, +1)
 )
-coal = processes.create("coal", (concrete, -5), (energy, +1))
-energy_grid = processes.create("energy_grid", (energy, -1))
+coal = processes.create("coal", (concrete, -5), (energy, +1), (electronics, 0))
 
-c1 = RunRatioConstraint(wind, solar, processes, 6 / 5)
-c2 = RunRatioConstraint(solar, coal, processes, 3)
-energy_con = ResourceConstraint(energy, energy_grid, resources, processes, 20)
-constraints = [energy_con]
+
+
+wind_con = ResourceConstraint(energy, wind, resources, processes, 5)
+coal_con = ResourceConstraint(energy, coal, resources, processes, 5)
+solar_con = ResourceConstraint(energy, solar, resources, processes, 5)
+constraints = [wind_con, coal_con, solar_con]
+print(constraints)
 # Minimise run total
-objective = concrete_producer + steel_producer + aluminium_producer + electronics_producer + wind + solar + coal + energy_grid
+objective = concrete_producer + steel_producer + aluminium_producer + electronics_producer + wind + solar + coal + energy_producer
 #objective = concrete_producer + steel_producer + aluminium_producer + electronics_producer # TODO Fix this bug
-objective = wind + solar + coal + energy_grid
+#objective = wind + solar + coal + energy_grid
 solution = Measure(resources, processes, constraints, objective)
-#print(solution._run_vector)
+print(solution._run_vector)
 result = solution.resource(energy)
 print(result)
