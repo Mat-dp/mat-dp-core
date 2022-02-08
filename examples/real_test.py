47532071d1c7604ea6279f2143f1813aeb6bb9fb
mark-todd <markpeter.todd@hotmail.co.uk>
2022-02-08 18:32:57 +0000

    Fixed formatting in examples

--- examples/real_test.py
+++ examples/real_test.py
@@ -19,7 +19,9 @@ processes = Processes()
 concrete_producer = processes.create("concrete_producer", (concrete, +1))
 steel_producer = processes.create("steel_producer", (steel, +1))
 aluminium_producer = processes.create("aluminium_producer", (aluminium, +1))
-electronics_producer = processes.create("electronics_producer", (electronics, +1))
+electronics_producer = processes.create(
+    "electronics_producer", (electronics, +1)
+)
 wind = processes.create("wind", (aluminium, -2), (concrete, -3), (energy, +1))
 solar = processes.create(
     "solar", (steel, -1), (electronics, -1), (aluminium, -0.5), (energy, +1)
@@ -32,10 +34,19 @@ c2 = RunRatioConstraint(solar, coal, processes, 3)
 energy_con = ResourceConstraint(energy, energy_grid, resources, processes, 20)
 constraints = [energy_con]
 # Minimise run total
-objective = concrete_producer + steel_producer + aluminium_producer + electronics_producer + wind + solar + coal + energy_grid
-#objective = concrete_producer + steel_producer + aluminium_producer + electronics_producer # TODO Fix this bug
+objective = (
+    concrete_producer
+    + steel_producer
+    + aluminium_producer
+    + electronics_producer
+    + wind
+    + solar
+    + coal
+    + energy_grid
+)
+# objective = concrete_producer + steel_producer + aluminium_producer + electronics_producer # TODO Fix this bug
 objective = wind + solar + coal + energy_grid
 solution = Measure(resources, processes, constraints, objective)
-#print(solution._run_vector)
+# print(solution._run_vector)
 result = solution.resource(energy)
 print(result)
