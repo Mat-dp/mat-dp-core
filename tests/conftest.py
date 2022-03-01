52b4b461d6b9586614fc0c22f573cf31292ca2bd
mark-todd <markpeter.todd@hotmail.co.uk>
2022-03-01 15:12:30 +0000

    Added more advanced tests of maths beghind measure

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,5 +37,77 @@ def farming_example_measure(farming_example) -> Measure:
     constraints = [
         EqConstraint("burger_consumption", processes["dairy_farm"], 10)
     ]
-    objective = -processes["arable_farm"]
+    objective = processes["arable_farm"]
+    return Measure(resources, processes, constraints, objective=objective)
+
+
+@pytest.fixture
+def pizza_example() -> Tuple[Resources, Processes]:
+    resources = Resources()
+    cardboard = resources.create("cardboard", unit="m2")
+    recycled_cardboard = resources.create("recycled_cardboard", unit="m2")
+    pizza_box = resources.create("pizza_box")
+    energy = resources.create("energy", unit="kWh")
+
+    processes = Processes()
+    processes.create("cardboard_producer", (cardboard, +1))
+    processes.create("recycled_cardboard_producer", (recycled_cardboard, +1))
+    processes.create(
+        "pizza_box_producer",
+        (recycled_cardboard, -0.5),
+        (cardboard, -2),
+        (pizza_box, 1),
+    )
+    processes.create(
+        "recycled_pizza_box_producer",
+        (recycled_cardboard, -3),
+        (cardboard, -1),
+        (pizza_box, 1),
+    )
+    processes.create("power_plant", (pizza_box, -1), (energy, 4))
+    processes.create("energy_grid", (energy, -2))
+
+    return resources, processes
+
+
+@pytest.fixture
+def pizza_example_measure(pizza_example) -> Measure:
+    resources, processes = pizza_example
+    constraints = [
+        EqConstraint(
+            "recycled pizza box ratio",
+            processes["pizza_box_producer"]
+            - processes["recycled_pizza_box_producer"],
+            0,
+        ),
+        EqConstraint("required_energy", processes["energy_grid"], 8),
+    ]
+    return Measure(resources, processes, constraints, objective=None)
+
+
+@pytest.fixture
+def parallel_farming_example() -> Tuple[Resources, Processes]:
+    resources = Resources()
+    hay = resources.create("hay")
+    cow = resources.create("cow")
+    grass = resources.create("grass")
+    sheep = resources.create("sheep")
+    processes = Processes()
+    processes.create("arable_farm", (hay, +1))
+    processes.create("dairy_farm", (cow, +1), (hay, -2))
+    processes.create("mcdonalds", (cow, -1))
+    processes.create("field_growth", (grass, +1))
+    processes.create("sheep_farm", (grass, -1), (sheep, +3))
+    processes.create("dog_food_factory", (sheep, -3))
+    return resources, processes
+
+
+@pytest.fixture
+def parallel_farming_example_measure(parallel_farming_example) -> Measure:
+    resources, processes = parallel_farming_example
+    constraints = [
+        EqConstraint("burger_consumption", processes["mcdonalds"], 10),
+        EqConstraint("dog_food_harvest", processes["dog_food_factory"], 10),
+    ]
+    objective = processes["arable_farm"]
     return Measure(resources, processes, constraints, objective=objective)
--- tests/test_maths.py
+++ tests/test_maths.py
@@ -5,17 +5,26 @@ from mat_dp_core import Measure
 
 
 @pytest.mark.asyncio
-class TestCalculation:
-    async def test_farming_all(self, farming_example_measure: Measure):
-        resource_matrix = farming_example_measure.resource_matrix
-        flow_matrix = farming_example_measure.flow_matrix
-        cumulative_resource_matrix = (
-            farming_example_measure.cumulative_resource_matrix
+class TestFarming:
+    async def test_run_vector(self, farming_example_measure: Measure):
+        run_vector = farming_example_measure._run_vector
+        assert np.all(
+            np.round(run_vector, decimals=3)
+            == np.array(
+                [20.0, 10.0, 10.0],
+                dtype=float,
+            )
         )
+
+    async def test_resource_matrix(self, farming_example_measure: Measure):
+        resource_matrix = farming_example_measure.resource_matrix
         assert np.all(
             np.round(resource_matrix, decimals=3)
             == np.array([[20, -20.0, 0.0], [0.0, 10.0, -10.0]], dtype=float)
         )
+
+    async def test_flow_matrix(self, farming_example_measure: Measure):
+        flow_matrix = farming_example_measure.flow_matrix
         assert np.all(
             np.round(flow_matrix, decimals=3)
             == np.array(
@@ -34,7 +43,199 @@ class TestCalculation:
                 dtype=float,
             )
         )
+
+    async def test_cumulative_resource_matrix(
+        self, farming_example_measure: Measure
+    ):
+        cumulative_resource_matrix = (
+            farming_example_measure.cumulative_resource_matrix
+        )
         assert np.all(
             np.round(cumulative_resource_matrix, decimals=3)
             == np.array([[20, 20.0, 20.0], [10.0, 10.0, 10.0]], dtype=float)
+        )
+
+
+@pytest.mark.asyncio
+class TestPizza:
+    async def test_run_vector(self, pizza_example_measure: Measure):
+        run_vector = pizza_example_measure._run_vector
+        assert np.all(
+            np.round(run_vector, decimals=3)
+            == np.array(
+                [6.0, 7.0, 2.0, 2.0, 4.0, 8.0],
+                dtype=float,
+            )
+        )
+
+    async def test_resource_matrix(self, pizza_example_measure: Measure):
+        resource_matrix = pizza_example_measure.resource_matrix
+
+        assert np.all(
+            np.round(resource_matrix, decimals=3)
+            == np.array(
+                [
+                    [6.0, 0.0, -4.0, -2.0, 0.0, 0.0],
+                    [0.0, 7.0, -1.0, -6.0, 0.0, 0.0],
+                    [0.0, 0.0, 2.0, 2.0, -4.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 16.0, -16.0],
+                ],
+                dtype=float,
+            )
+        )
+
+    async def test_flow_matrix(self, pizza_example_measure: Measure):
+        flow_matrix = pizza_example_measure.flow_matrix
+        assert np.all(
+            np.round(flow_matrix, decimals=3)
+            == np.array(
+                [
+                    [
+                        [0.0, 0.0, 4.0, 2.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [-4.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [-2.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    ],
+                    [
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 1.0, 6.0, 0.0, 0.0],
+                        [0.0, -1.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, -6.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    ],
+                    [
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 2.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 2.0, 0.0],
+                        [0.0, 0.0, -2.0, -2.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    ],
+                    [
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 16.0],
+                        [0.0, 0.0, 0.0, 0.0, -16.0, 0.0],
+                    ],
+                ],
+                dtype=float,
+            )
+        )
+
+    async def test_cumulative_resource_matrix(
+        self, pizza_example_measure: Measure
+    ):
+        cumulative_resource_matrix = (
+            pizza_example_measure.cumulative_resource_matrix
+        )
+        assert np.all(
+            np.round(cumulative_resource_matrix, decimals=3)
+            == np.array(
+                [
+                    [6.0, 6.0, 6.0, 6.0, 6.0, 6.0],
+                    [7.0, 7.0, 7.0, 7.0, 7.0, 7.0],
+                    [4.0, 4.0, 4.0, 4.0, 4.0, 4.0],
+                    [16.0, 16.0, 16.0, 16.0, 16.0, 16.0],
+                ],
+                dtype=float,
+            )
+        )
+
+
+@pytest.mark.asyncio
+class TestParallelFarming:
+    async def test_run_vector(self, parallel_farming_example_measure: Measure):
+        run_vector = parallel_farming_example_measure._run_vector
+        assert np.all(
+            np.round(run_vector, decimals=3)
+            == np.array(
+                [20.0, 10.0, 10.0, 10.0, 10.0, 10.0],
+                dtype=float,
+            )
+        )
+
+    async def test_resource_matrix(
+        self, parallel_farming_example_measure: Measure
+    ):
+        resource_matrix = parallel_farming_example_measure.resource_matrix
+        assert np.all(
+            np.round(resource_matrix, decimals=3)
+            == np.array(
+                [
+                    [20.0, -20.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 10.0, -10.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 10.0, -10.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 30.0, -30.0],
+                ],
+                dtype=float,
+            )
+        )
+
+    async def test_flow_matrix(
+        self, parallel_farming_example_measure: Measure
+    ):
+        flow_matrix = parallel_farming_example_measure.flow_matrix
+        assert np.all(
+            np.round(flow_matrix, decimals=3)
+            == np.array(
+                [
+                    [
+                        [0.0, 20.0, 0.0, 0.0, 0.0, 0.0],
+                        [-20.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    ],
+                    [
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 10.0, 0.0, 0.0, 0.0],
+                        [0.0, -10.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    ],
+                    [
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 10.0, 0.0],
+                        [0.0, 0.0, 0.0, -10.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    ],
+                    [
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 30.0],
+                        [0.0, 0.0, 0.0, 0.0, -30.0, 0.0],
+                    ],
+                ],
+                dtype=float,
+            )
+        )
+
+    async def test_cumulative_resource_matrix(
+        self, parallel_farming_example_measure: Measure
+    ):
+        cumulative_resource_matrix = (
+            parallel_farming_example_measure.cumulative_resource_matrix
+        )
+        assert np.all(
+            np.round(cumulative_resource_matrix, decimals=3)
+            == np.array(
+                [
+                    [20.0, 20.0, 20.0, 0.0, 0.0, 0.0],
+                    [10.0, 10.0, 10.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 10.0, 10.0, 10.0],
+                    [0.0, 0.0, 0.0, 30.0, 30.0, 30.0],
+                ],
+                dtype=float,
+            )
         )
